0ee3325d47d540202ab05d65739c54c6cd99f779
root <root@fieldhousem2.mylabserver.com>
2017-06-12 12:23:57 +0000

    updated webdev

--- manifests/webusers.pp
+++ manifests/webusers.pp
@@ -1,13 +1,8 @@
 class profile::webusers(
   Hash $create_users
 ) {
-  include webdev
+  include webdev::create_users
 
-  $defaults = {
-    ensure   => present,
-    managehome => true,
-    shell      => '/bin/bash',
-  }
-  create_resources(::webdev::create_users, $create_users, $defaults)
+  create_resources(::webdev::create_users, $create_users)
 }
 
